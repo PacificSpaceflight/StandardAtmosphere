da0490286aae03ac351fa4662329e4a590beecce
robbykraft <robbykraft@gmail.com>
2015-04-06 02:57:53 -0500

    formatting

--- interface.js
+++ interface.js
@@ -65,60 +65,15 @@ if (window.devicePixelRatio > 1) {
 // }
 
 
-function drawSkyLines(mouse){
+function drawMovingStats(mouse){
 	canvas.width = canvas.width;
 	context.lineWidth = 1;
 	context.lineCap = "round";
-
-	// light gray fill, visible segment piece
-	// context.fillStyle = "#E0E0E0";
-	// context.beginPath();
-	// context.moveTo(o.x + p1.x*scale, o.y + p1.y*scale);
-	// for(var i = 0; i < points.length; i++) context.lineTo(o.x + points[i].x*scale, o.y + points[i].y*scale);
-	// context.lineTo(o.x + p1.x*scale, o.y + p1.y*scale);
-	// context.fill();
-
-	// 2 mouse ray lines
-	// context.strokeStyle = "#E0E0E0";
-	// context.beginPath();
-	// context.moveTo(o.x + mouse.x, o.y + mouse.y);
-	// context.lineTo(o.x + mouse.x+scale*2*dRayR.x, o.y + mouse.y+scale*2*dRayR.y);
-	// context.moveTo(o.x + mouse.x, o.y + mouse.y);
-	// context.lineTo(o.x + mouse.x+scale*2*dRayL.x, o.y + mouse.y+scale*2*dRayL.y);
-	// context.stroke();
-
-	// 1/12 full segment- light gray outline
-	// context.strokeStyle = "#E0E0E0";
-	// context.beginPath();
-	// context.moveTo(o.x + p1.x*scale, o.y + p1.y*scale);
-	// context.lineTo(o.x + p2.x*scale, o.y + p2.y*scale);
-	// context.lineTo(o.x + p3.x*scale, o.y + p3.y*scale);
-	// context.lineTo(o.x + p1.x*scale, o.y + p1.y*scale);
-	// context.stroke();
-
-	// 1/12 visible segment- black outline
 	context.strokeStyle = "#FFFFFF";
 	context.beginPath();
 	context.moveTo(0, mouse.y * canvas.height);
-	context.lineTo(canvas.width * .1, mouse.y * canvas.height);
+	context.lineTo(canvas.width * .033, mouse.y * canvas.height);
 	context.stroke();
-
-
-	// for(var i = 0; i < points.length; i++){
-	// 	context.lineTo(o.x + points[i].x*scale, o.y + points[i].y*scale);
-	// }
-
-	// HEXAGON SNOWFLAKE RESULT
- //  	hexCanvas.width = hexCanvas.width;
-	// hexContext.lineWidth = 4;
-	// hexContext.lineCap = "round";
-
-	// var hexPoints = hexagonFromTwelfthHexagon(points);
-	// hexContext.moveTo(hexCenter.x + hexPoints[0].x*hexScale, hexCenter.y + hexPoints[0].y*hexScale);
-	// for(var i = 1; i < hexPoints.length; i++){
-	// 	hexContext.lineTo(hexCenter.x + hexPoints[i].x*hexScale, hexCenter.y + hexPoints[i].y*hexScale);
-	// }
-	// hexContext.stroke();
 }
 
 $("#interface").mousemove(function(event){
@@ -132,21 +87,21 @@ $("#interface").mousemove(function(event){
 	document.getElementById("stats").innerHTML = "<table><tr><td>" + "<h3>" + 
 			a.temperature.toFixed(2) + " &deg;C</h3><h3>" + 
 			a.pressure.toFixed(2) + " psi </h3><h3>" +
-			a.density.toFixed(2) + " kg/m^3</h3>" + "</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td style='color:#C0C0C0';><h3>" + 
+			a.density.toFixed(2) + " kg/m^3</h3>" + "</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td style='color:#d0d0d0';><h3>" + 
 			"&Delta; " + tempDiff.toFixed(2) + "&deg;C</h3><h3>" + 
 			"&Delta; " + pressDiff.toFixed(2) + " psi</h3><h3>" +
 			"&Delta; " + densDiff.toFixed(2) + " kg/m^3</h3><h3>" + 
 			"&Delta; from sea level</h3></td></tr></table>";
-	drawSkyLines( {x:event.offsetX / window.innerWidth, y:event.offsetY / window.innerHeight} );
+	drawMovingStats( {x:event.offsetX / window.innerWidth, y:event.offsetY / window.innerHeight} );
 
 	var bump = 0;
 	if(event.offsetY / window.innerHeight > .75)
 		bump = -100;
 
-	document.getElementById("stats").style.left = canvas.width * .05 + "px"; 
+	document.getElementById("stats").style.left = canvas.width * .04 + "px"; 
 	document.getElementById("stats").style.top = 20+event.offsetY + bump + "px"
 
-	document.getElementById('elevationText').style.left = canvas.width * .11 + "px";
+	document.getElementById('elevationText').style.left = canvas.width * .04 + "px";
 	document.getElementById('elevationText').style.top = event.offsetY -10 + "px";
 	document.getElementById('elevationText').innerHTML = "<h2>" + altitude.toFixed(2) + " meters</h2>";
 });
